b1cba80a1bbc39393a571ca80d4e8efd1858fdb1
GFORTI <GFORTI@N210INSTR.labs.neit.edu>
2015-04-14 19:17:53 -0400

    MVC

---
+++ week3/lab/site/mvc/views/test.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        
+        
+        <h1>Test</h1>
+        
+        
+        <?php
+        
+        
+        var_dump($scope->view);
+        
+        
+        ?>
+    </body>
+</html>
