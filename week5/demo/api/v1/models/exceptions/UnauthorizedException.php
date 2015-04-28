99d0e12cc8a68dd878a65db810744cb55736fd8d
Gabriel Forti <gnforti@gmail.com>
2015-04-28 08:21:45 -0400

    demos for rest

---
+++ week5/demo/api/v1/models/exceptions/UnauthorizedException.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Description of UnauthorizedException
+ *
+ * @author User
+ */
+namespace App\models\services;
+
+use Exception;
+class UnauthorizedException extends Exception {}
+
---
+++ week5/demo/proxyAPI.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\models\services;
+
+include_once './api/v1/models/interfaces/IService.php';
+include_once './api/v1/models/helpers/RestProxy.php';
+
+$consumeAPI = new RestProxy();
+
+$key = 'test';
+$auth = $key.':';
+
+$method = $consumeAPI->getHTTPVerb();
+$data = $consumeAPI->getHTTPData();
+
+$url = $consumeAPI->endpoint('http://localhost/PHPadvClassSpring2015/week5/demo/api/v1');
+
+$consumeAPI->callAPI($method, $url, $data, $auth);
+
